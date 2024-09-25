3ec2a10e7daa4c51060d39dfacba4a7441e25d56
Tridots Tech <info@tridotstech.com>
2024-09-25 14:36:36 +0000

    Checking video recording

--- go1_recruit/templates/pages/candidatepage.py
+++ go1_recruit/templates/pages/candidatepage.py
@@ -26,6 +26,7 @@ def get_context(context):
 	if frappe.form_dict.code:
 		context.code=frappe.form_dict.code
 	url = '{0}/online_interview1?token={1}'.format(frappe.utils.get_url(), Token)
+	frappe.log_error("cand_url",url)
 	encrypt_url=frappe.db.sql('''select name, questionpaper_id, meeting_id, candidate_name, candidate_email, test_attempted, time_zone, start_time, end_time, monitored_test, candidate_video, record_screen, interviewer_email  from `tabQuestion Paper Candidates` where encrypted_url=%(token)s''',{'token':url},as_dict=1)
 	QuestionId=encrypt_url[0].questionpaper_id
 	exam_result_id = frappe.db.get_all('Exam Result',fields=['exam_id'],filters={'user':encrypt_url[0].candidate_email})
